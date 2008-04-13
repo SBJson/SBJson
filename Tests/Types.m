108b9ca38dc2fd8ad31db078cfc71b3b4ab10d13
Stig Brautaset <stig@brautaset.org>
2008-04-13 11:27:25 +0000

    r2659@russet (orig r2608):  stig | 2007-09-11 01:05:35 +0100  initial import

---
+++ Tests/Types.m
@@ -0,0 +1,14 @@
+//
+//  Types.m
+//  JSON
+//
+//  Created by Stig Brautaset on 11/09/2007.
+//  Copyright 2007 Stig Brautaset. All rights reserved.
+//
+
+#import "Types.h"
+
+
+@implementation Types
+
+@end
