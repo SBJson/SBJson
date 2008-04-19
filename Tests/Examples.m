81846ef9db953a03dc887248c6000cea97534260
Stig Brautaset <stig@brautaset.org>
2008-04-19 19:24:33 +0000

    Iterate over examples in a less convoluted way.

--- Tests/Examples.m
+++ Tests/Examples.m
@@ -6,96 +6,92 @@
 //  Copyright 2007 Stig Brautaset. All rights reserved.
 //
 
-// The rfc4627a and rfc4627b are examples are from RFC4627, the JSON RFC.
-//
-// The other examples are from http://www.json.org/example.html .
-
 #import "Tests.h"
-
-#define verifyExample(x) \
-    do {\
-        id expected = [self plistExample:x]; \
-        eqo([self jsonExample:x], expected); \
-        eqo([[expected JSONRepresentation] JSONValue], expected); \
-    } while (0)
 
 @implementation Examples
 
-- (NSString *)exampleFromFile:(NSString *)name
-{
-    id path = [@"Tests/Examples" stringByAppendingPathComponent:name];
-    id string = [NSString stringWithContentsOfFile:path 
-                                          encoding:NSASCIIStringEncoding
-                                             error:nil];
-    STAssertNotNil(string, @"Failed loading string from file: %@", path);
-    return string;
-}
+- (void)testJsonOrg {
+    NSString *file, *dir = @"Tests/json.org";
+    NSDirectoryEnumerator *files = [[NSFileManager defaultManager] enumeratorAtPath:dir];
 
-- (id)plistExample:(NSString *)path
-{
-    return [[self exampleFromFile: [path stringByAppendingString:@".plist"]] propertyList];
-}
+    while (file = [files nextObject]) {
+        if ([[file pathExtension] isEqualToString:@"plist"])
+            continue;
+        
+        NSString *jsonPath = [dir stringByAppendingPathComponent:file];
+        NSString *plistPath = [[jsonPath stringByDeletingPathExtension] stringByAppendingPathExtension:@"plist"];
+        
+        NSString *jsonRep = [NSString stringWithContentsOfFile:jsonPath
+                                                      encoding:NSASCIIStringEncoding
+                                                         error:nil];
+        id json;
+        STAssertNoThrow(json = [jsonRep JSONValue], nil);
+        STAssertNotNil(json, nil);
+        
+        // Check that we roundtrip properly
+        STAssertEqualObjects([[json JSONRepresentation] JSONValue], json, nil);
 
-- (id)jsonExample:(NSString *)path
-{
-    return [[self exampleFromFile: [path stringByAppendingString:@".json"]] JSONValue];
-}
+        NSString *plist = [NSString stringWithContentsOfFile:plistPath
+                                                    encoding:NSASCIIStringEncoding
+                                                       error:nil];
+        
+        if (!plist)
+            continue;
 
-- (void)testRFC4627a
-{
-    verifyExample(@"rfc4627a");
-}
-
-- (void)testRFC4627b
-{
-    verifyExample(@"rfc4627b");
-}
-
-- (void)testExample1
-{
-    verifyExample(@"ex1");
-}
-
-- (void)testExample2
-{
-    verifyExample(@"ex2");
-}
-
-- (void)testExample3
-{
-    verifyExample(@"ex3");
-}
-
-- (void)testExample4
-{
-    verifyExample(@"ex4");
-}
-
-- (void)testExample5
-{
-    // The fifth example on json.org/example.html cannot be
-    // represented as a property list. Or something. I believe the
-    // embedded nulls are the culprits.
-    STAssertNotNil([self jsonExample:@"ex5"], nil);
-}
-
-- (void)testJSONCheckerFail
-{
-    id opts = [NSDictionary dictionaryWithObject:@"19" forKey:@"MaxDepth"];
-    id o;
-    for (int i = 1; i < 34; i++) {
-        NSString *name = [NSString stringWithFormat:@"JSONChecker/fail%u.json", i];
-        NSString *json = [self exampleFromFile:name];
-        STAssertThrows(o = [json JSONValueWithOptions:opts], @"test %@ (%@) returned: %@", name, json, o);
+        id expected = [plist propertyList];
+        STAssertEqualObjects(json, expected, nil);
+        STAssertEqualObjects([[expected JSONRepresentation] JSONValue], expected, nil);
     }
 }
 
-- (void)testJSONCheckerPass
-{
-    for (int i = 1; i < 4; i++) {
-        NSString *name = [NSString stringWithFormat:@"JSONChecker/pass%u.json", i];
-        NSString *json = [self exampleFromFile:name];
-        STAssertNotNil([json JSONValue], @"test %@ (%@) passed ", name, json);
+- (void)testRFC4627 {
+    NSString *file, *dir = @"Tests/rfc4627";
+    NSDirectoryEnumerator *files = [[NSFileManager defaultManager] enumeratorAtPath:dir];
+
+    while (file = [files nextObject]) {
+        if ([[file pathExtension] isEqualToString:@"plist"])
+            continue;
+        
+        NSString *jsonPath = [dir stringByAppendingPathComponent:file];
+        NSString *plistPath = [[jsonPath stringByDeletingPathExtension] stringByAppendingPathExtension:@"plist"];
+        
+        NSString *jsonRep = [NSString stringWithContentsOfFile:jsonPath
+                                                      encoding:NSASCIIStringEncoding
+                                                         error:nil];
+        id json;
+        STAssertNoThrow(json = [jsonRep JSONValue], nil);
+        STAssertNotNil(json, nil);
+        
+        // Check that we roundtrip properly
+        STAssertEqualObjects([[json JSONRepresentation] JSONValue], json, nil);
+        
+        NSString *plist = [NSString stringWithContentsOfFile:plistPath
+                                                    encoding:NSASCIIStringEncoding
+                                                       error:nil];
+        
+        id expected = [plist propertyList];
+        STAssertEqualObjects(json, expected, nil);
+        STAssertEqualObjects([[expected JSONRepresentation] JSONValue], expected, nil);
+    }
+}
+
+
+- (void)testJsonChecker {
+    NSString *file, *dir = @"Tests/jsonchecker";
+    NSDirectoryEnumerator *files = [[NSFileManager defaultManager] enumeratorAtPath:dir];
+    
+    NSDictionary *options = [NSDictionary dictionaryWithObject:@"19" forKey:@"MaxDepth"];
+    while (file = [files nextObject]) {
+        NSString *json = [NSString stringWithContentsOfFile:[dir stringByAppendingPathComponent:file]
+                                                   encoding:NSASCIIStringEncoding
+                                                      error:nil];
+
+        if ([file hasPrefix:@"pass"]) {
+            STAssertNoThrow([json JSONValueWithOptions:options], nil);
+            
+        } else {
+            STAssertThrows([json JSONValueWithOptions:options], nil);
+        }
     }
 }
 
