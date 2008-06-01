3fabf3c7f7c92cf97eb3b2992b2a9f89955fd9d4
Stig Brautaset <stig@brautaset.org>
2008-06-01 23:15:57 +0000

    redict to frontpage of documentation

--- dmg.sh
+++ dmg.sh
@@ -16,4 +16,22 @@ xcodebuild -configuration Release -target JSON install
 mkdir $DIST
 cp -p -R /tmp/Frameworks/$PROJ.framework $DIST/
 cp -p -R Doxygen.docset/html $DIST/html
+
+cat <<HTML > $DIST/API.html
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<script type="text/javascript">
+<!--
+window.location = "html/index.html"
+//-->
+</script>
+</head>
+<body>
+<p>Aw, shucks! I tried to redirect you to the <a
+href="html/index.html">api documentation</a> but obviously
+failed. Please find it yourself. </p>
+</body>
+</html>
+HTML
+
 hdiutil create -fs HFS+ -volname $DIST -srcfolder $DIST $DMG
