ea33bef928bc117803c3c2e0fd78079e55f2c3d6
Stig Brautaset <stig@brautaset.org>
2009-03-16 19:48:26 +0000

    First step in refactoring to split the JSON blob-class into a reader and a writer. Extract interface.

--- Source/SBJSON.h
+++ Source/SBJSON.h
@@ -28,23 +28,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #import <Foundation/Foundation.h>
+#import "SBJsonParser.h"
 
 extern NSString * SBJSONErrorDomain;
-
-enum {
-    EUNSUPPORTED = 1,
-    EPARSENUM,
-    EPARSE,
-    EFRAGMENT,
-    ECTRL,
-    EUNICODE,
-    EDEPTH,
-    EESCAPE,
-    ETRAILCOMMA,
-    ETRAILGARBAGE,
-    EEOF,
-    EINPUT
-};
 
 /**
 @brief A strict JSON parser and generator
@@ -84,7 +70,7 @@ in arrays or objects. Nor does it support embedded comments, or
 anything else not in the JSON specification.
  
 */
-@interface SBJSON : NSObject {
+@interface SBJSON : NSObject <SBJsonParser> {
     BOOL humanReadable;
     BOOL sortKeys;
     NSUInteger maxDepth;
@@ -108,30 +94,15 @@ anything else not in the JSON specification.
 /** The default is to not sort the keys. */
 @property BOOL sortKeys;
 
-/// The maximum depth the parser will go to
-/** Defaults to 512. */
-@property NSUInteger maxDepth;
-
 /// Return JSON representation of an array  or dictionary
 - (NSString*)stringWithObject:(id)value error:(NSError**)error;
 
 /// Return JSON representation of any legal JSON value
 - (NSString*)stringWithFragment:(id)value error:(NSError**)error;
 
-/// Return the object represented by the given string
-- (id)objectWithString:(NSString*)jsonrep error:(NSError**)error;
-
-/// Return the fragment represented by the given string
-- (id)fragmentWithString:(NSString*)jsonrep error:(NSError**)error;
-
 /// Return JSON representation (or fragment) for the given object
 - (NSString*)stringWithObject:(id)value
                   allowScalar:(BOOL)x
     					error:(NSError**)error;
-
-/// Parse the string and return the represented object (or scalar)
-- (id)objectWithString:(id)value
-           allowScalar:(BOOL)x
-    			 error:(NSError**)error;
 
 @end
---
+++ Source/SBJsonParser.m
@@ -0,0 +1,14 @@
+//
+//  SBJsonParser.m
+//  JSON
+//
+//  Created by Stig Brautaset on 16/03/2009.
+//  Copyright 2009 Stig Brautaset. All rights reserved.
+//
+
+#import "SBJsonParser.h"
+
+
+@implementation SBJsonParser
+
+@end
