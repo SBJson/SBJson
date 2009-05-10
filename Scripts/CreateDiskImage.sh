0bd600539ab2899320faed1ae03c4c0d537f8795
Stig Brautaset <stig@brautaset.org>
2009-05-10 00:32:46 +0000

    new target to generate release disk image

---
+++ Scripts/CreateDiskImage.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+set -x
+
+# Determine the project name and version
+VERS=$(agvtool mvers -terse1)
+
+# Derived names
+VOLNAME=${PROJECT}_${VERS}
+DISK_IMAGE=$BUILD_DIR/$VOLNAME
+DISK_IMAGE_FILE=$INSTALL_DIR/$VOLNAME.dmg
+
+# Remove old targets
+rm -f $DISK_IMAGE_FILE
+test -d $DISK_IMAGE && chmod -R +w $DISK_IMAGE && rm -rf $DISK_IMAGE
+mkdir -p $DISK_IMAGE
+
+# Create the Embedded framework
+xcodebuild -target JSON -configuration Release install || exit 1
+cp -p -R /tmp/Frameworks/$PROJECT.framework $DISK_IMAGE
+
+# Copy the source verbatim into the DISK_IMAGE_FILE
+cp -p -R $SOURCE_ROOT/Source $DISK_IMAGE/$PROJECT
+
+# Create the documentation
+xcodebuild -target Documentation -configuration Release install || exit 1
+cp -p -R $INSTALL_DIR/Docset/html $DISK_IMAGE/Documentation
+rm -f $DISK_IMAGE/Documentation/Makefile
+rm -f $DISK_IMAGE/Documentation/Info.plist
+rm -f $DISK_IMAGE/Documentation/Nodes.xml
+rm -f $DISK_IMAGE/Documentation/Tokens.xml
+rm -rf $DISK_IMAGE/Documentation/org.brautaset.$PROJECT.docset
+
+cat <<HTML > $DISK_IMAGE/Documentation.html
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<script type="text/javascript">
+<!--
+window.location = "Documentation/index.html"
+//-->
+</script>
+</head>
+<body>
+<p>Aw, shucks! I tried to redirect you to the <a
+href="Documentaton/html/index.html">api documentation</a> but obviously
+failed. Please find it yourself. </p>
+</body>
+</html>
+HTML
+
+cp -p $SOURCE_ROOT/CREDITS $DISK_IMAGE
+cp -p $SOURCE_ROOT/README $DISK_IMAGE
+cp -p $SOURCE_ROOT/Install.rtf $DISK_IMAGE
+
+# hdiutil create -fs HFS+ -volname $VOLNAME -srcfolder $DISK_IMAGE $DISK_IMAGE_FILE
---
+++ Scripts/InstallDocumentation.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+# See also http://developer.apple.com/tools/creatingdocsetswithdoxygen.html 
+
+set -x
+
+VERSION=$(agvtool mvers -terse1)
+
+DOXYFILE=$DERIVED_FILES_DIR/doxygen.config
+DOXYGEN=/Applications/Doxygen.app/Contents/Resources/doxygen
+DOCSET=$INSTALL_DIR/Docset
+
+mkdir -p $DERIVED_FILES_DIR || exit 1
+mkdir -p $DOCSET || exit 1
+
+if ! test -x $DOXYGEN ; then
+	echo "*** Install Doxygen to get documentation generated for you automatically ***"
+	exit 1
+fi
+
+# Create a doxygen configuration file with only the settings we care about
+$DOXYGEN -g $DOXYFILE
+
+cat <<EOF >> $DOXYFILE
+
+PROJECT_NAME           = $FULL_PRODUCT_NAME
+PROJECT_NUMBER         = $VERSION
+OUTPUT_DIRECTORY       = $DOCSET
+INPUT                  = $SOURCE_ROOT/Source
+FILE_PATTERNS          = *.h *.m
+
+HIDE_UNDOC_MEMBERS     = YES
+HIDE_UNDOC_CLASSES     = YES
+HIDE_UNDOC_RELATIONS   = YES
+REPEAT_BRIEF           = NO
+
+GENERATE_LATEX         = NO
+GENERATE_HTML          = YES
+GENERATE_DOCSET        = YES
+DOCSET_FEEDNAME        = "brautaset.org $PROJECT Documentation"
+DOCSET_BUNDLE_ID       = org.brautaset.$PROJECT
+
+EOF
+
+#  Run doxygen on the updated config file.
+#  doxygen creates a Makefile that does most of the heavy lifting.
+$DOXYGEN $DOXYFILE
+
+#  make will invoke docsetutil. Take a look at the Makefile to see how this is done.
+make -C $DOCSET/html install
+
+#  Construct a temporary applescript file to tell Xcode to load a docset.
+rm -f $TEMP_DIR/loadDocSet.scpt
+
+cat <<EOF > $TEMP_DIR/loadDocSet.scpt
+tell application "Xcode"
+	load documentation set with path "/Users/$USER/Library/Developer/Shared/Documentation/DocSets/org.brautaset.${PROJECT}.docset/"
+end tell
+EOF
+
+# Run the load-docset applescript command.
+osascript $TEMP_DIR/loadDocSet.scpt
