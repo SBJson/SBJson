1b65cc7d5a3e777753c2af314cc76a402c8767a9
Stig Brautaset <stig@brautaset.org>
2010-06-20 18:24:45 +0100

    converted pretty-printing tests to be more generic -- rest of tests to follow

--- Tests/Examples.m
+++ Tests/Examples.m
@@ -1,15 +1,83 @@
-//
-//  Examples.m
-//  JSON
-//
-//  Created by Stig Brautaset on 13/09/2007.
-//  Copyright 2007 Stig Brautaset. All rights reserved.
-//
+/*
+ Copyright (C) 2007-2010 Stig Brautaset. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ * Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ 
+ * Neither the name of the author nor the names of its contributors may be used
+   to endorse or promote products derived from this software without specific
+   prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 
 #import "Examples.h"
 #import <JSON/JSON.h>
 
 @implementation Examples
+
+- (void)setUp {
+    parser = [SBJsonParser new];
+    writer = [SBJsonWriter new];
+    writer.humanReadable = YES;
+    writer.sortKeys = YES;
+    
+}
+
+- (void)testFiles {
+    
+    NSString *file;
+    NSString *dir = @"Tests/Data";
+    NSDirectoryEnumerator *files = [[NSFileManager defaultManager] enumeratorAtPath:dir];
+    
+    while ((file = [files nextObject])) {
+        if (![[file pathExtension] isEqualToString:@"json"])
+            continue;
+        
+        NSString *jsonPath = [dir stringByAppendingPathComponent:file];        
+        NSString *jsonText = [NSString stringWithContentsOfFile:jsonPath
+                                                       encoding:NSASCIIStringEncoding
+                                                          error:nil];
+        STAssertNotNil(jsonText, @"Could not load %@", jsonPath);
+        
+        id parsed;
+        STAssertNoThrow(parsed = [parser objectWithString:jsonText], jsonPath);
+        STAssertNotNil(parsed, jsonPath);
+        STAssertNil(parser.errorTrace, jsonPath);
+        
+        NSString *written;
+        STAssertNoThrow(written = [writer stringWithObject:parsed], jsonPath);
+        STAssertNotNil(written, jsonPath);
+        STAssertNil(writer.errorTrace, nil);
+        
+        NSString *goldPath = [jsonPath stringByAppendingPathExtension:@"gold"];
+        NSString *goldText = [NSString stringWithContentsOfFile:goldPath
+                                                       encoding:NSASCIIStringEncoding
+                                                          error:nil];
+        STAssertNotNil(goldText, @"Could not load %@", goldPath);
+
+        // Chop off newline at end of string
+        goldText = [goldText substringToIndex:[goldText length]-1];
+        STAssertEqualObjects(written, goldText, @"%@: %@ != %@", jsonPath, jsonText, goldText);
+    }
+}
 
 - (void)testJsonOrg {
     NSString *file, *dir = @"Tests/json.org";
@@ -19,14 +87,14 @@
         if (![[file pathExtension] isEqualToString:@"json"])
             continue;
         
-        NSString *jsonPath = [dir stringByAppendingPathComponent:file];
-        NSString *plistPath = [[jsonPath stringByDeletingPathExtension] stringByAppendingPathExtension:@"plist"];
+        NSString *path = [dir stringByAppendingPathComponent:file];
+        NSString *plistPath = [[path stringByDeletingPathExtension] stringByAppendingPathExtension:@"plist"];
         
-        NSString *jsonRep = [NSString stringWithContentsOfFile:jsonPath
+        NSString *jsonText = [NSString stringWithContentsOfFile:path
                                                       encoding:NSASCIIStringEncoding
                                                          error:nil];
         id json;
-        STAssertNoThrow(json = [jsonRep JSONValue], nil);
+        STAssertNoThrow(json = [jsonText JSONValue], nil);
         STAssertNotNil(json, nil);
         
         // Check that we roundtrip properly
@@ -53,14 +121,14 @@
         if (![[file pathExtension] isEqualToString:@"json"])
             continue;
         
-        NSString *jsonPath = [dir stringByAppendingPathComponent:file];
-        NSString *plistPath = [[jsonPath stringByDeletingPathExtension] stringByAppendingPathExtension:@"plist"];
+        NSString *path = [dir stringByAppendingPathComponent:file];
+        NSString *plistPath = [[path stringByDeletingPathExtension] stringByAppendingPathExtension:@"plist"];
         
-        NSString *jsonRep = [NSString stringWithContentsOfFile:jsonPath
+        NSString *jsonText = [NSString stringWithContentsOfFile:path
                                                       encoding:NSASCIIStringEncoding
                                                          error:nil];
         id json;
-        STAssertNoThrow(json = [jsonRep JSONValue], nil);
+        STAssertNoThrow(json = [jsonText JSONValue], nil);
         STAssertNotNil(json, nil);
         
         // Check that we roundtrip properly
