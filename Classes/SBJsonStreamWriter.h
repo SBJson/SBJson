9f7252df55100cef6b4e04b233f3f65b0552d1b4
Stig Brautaset <stig@brautaset.org>
2010-10-24 00:30:06 +0100

    Add a minimum documentation for the stream writer

--- Classes/SBJsonStreamWriter.h
+++ Classes/SBJsonStreamWriter.h
@@ -35,6 +35,27 @@
 
 @class SBJsonStreamWriterStateMachine;
 
+/**
+ @brief The Stream Writer class.
+ 
+ SBJsonStreamWriter accepts various messages and writes JSON text to an
+ NSOutputStream designated at iniatilisation time. 
+ 
+ A range of high-, mid- and low-level methods. You can mix and match calls
+ to these. For example, you may want to call -writeArrayOpen to start an
+ array and then repeatedly call -writeObject: with an object.
+ 
+ In JSON the keys of an object must be strings. NSDictionary keys need
+ not be, but attempting to convert an NSDictionary with non-string keys
+ into JSON will result in an error.
+ 
+ NSNumber instances created with the +initWithBool: method are
+ converted into the JSON boolean "true" and "false" values, and vice
+ versa. Any other NSNumber instances are converted to a JSON number the
+ way you would expect.
+ 
+ */
+
 @interface SBJsonStreamWriter : SBJsonBase {
 @private
 	SBJsonStreamWriterStateMachine **states;
@@ -42,28 +63,70 @@
     BOOL sortKeys, humanReadable;
 }
 
+/**
+ @brief Whether we are generating human-readable (multiline) JSON.
+ 
+ Set whether or not to generate human-readable JSON. The default is NO, which produces
+ JSON without any whitespace between tokens. If set to YES, generates human-readable
+ JSON with linebreaks after each array value and dictionary key/value pair, indented two
+ spaces per nesting level.
+ */
 @property BOOL humanReadable;
+
+/**
+ @brief Whether or not to sort the dictionary keys in the output.
+ 
+ If this is set to YES, the dictionary keys in the JSON output will be in sorted order.
+ (This is useful if you need to compare two structures, for example.) The default is NO.
+ */
 @property BOOL sortKeys;
 
+/**
+ @brief Initialise a stream writer.
+ 
+ You have to create an output stream first. You should not open/close the  stream
+ manually; this class takes care of that.
+ */
 - (id)initWithStream:(NSOutputStream*)stream;
 
-// High level
+/// @deprecated Please don't use this.
 - (BOOL)write:(id)value;
 
-// Mid-level methods
+/**
+ @brief Write an NSDictionary to the JSON stream.
+ */
 - (BOOL)writeObject:(NSDictionary*)dict;
+
+/**
+ @brief Write an NSArray to the JSON stream.
+ */
 - (BOOL)writeArray:(NSArray *)array;
 
-// Low-level methods
+/// Start writing an Object to the stream
 - (BOOL)writeObjectOpen;
+
+/// Close the current object being written
 - (void)writeObjectClose;
+
+/// Start writing an Array to the stream
 - (BOOL)writeArrayOpen;
+
+/// Close the current Array being written
 - (void)writeArrayClose;
+
+/// Write a null to the stream
 - (BOOL)writeNull;
+
+/// Write a boolean to the stream
 - (BOOL)writeBool:(BOOL)x;
+
 //- (BOOL)writeInteger:(long)l;
 //- (BOOL)writeDouble:(double)d;
+
+/// Write a Number to the stream
 - (BOOL)writeNumber:(NSNumber*)n;
+
+/// Write a String to the stream
 - (void)writeString:(NSString*)s;
 
 @end
