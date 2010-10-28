9b358e7b74250dd4f43185d6b2d160bfc41c57fb
Sam Soffes <sam@samsoff.es>
2010-10-28 13:19:17 -0500

    Fixed warning

--- Classes/SBJsonStreamWriter.m
+++ Classes/SBJsonStreamWriter.m
@@ -34,7 +34,7 @@
 #import "SBProxyForJson.h"
 
 @interface SBJsonStreamWriter ()
-@property NSString *error;
+@property(assign) NSString *error;
 @property(readonly) NSObject **states;
 @property(readonly) NSUInteger depth;
 @property(readonly) NSOutputStream *stream;
