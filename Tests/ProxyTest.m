1a260c599461c0a9d4e6973348c7a20e12ca74f9
Stig Brautaset <stig@brautaset.org>
2010-12-01 22:21:05 +0000

    Autorelease to avoid static analysis error

--- Tests/ProxyTest.m
+++ Tests/ProxyTest.m
@@ -55,7 +55,7 @@
 
 @implementation Bool
 - (id)proxyForJson {
-    return [NSArray arrayWithObjects:[True new], [False new], nil];
+    return [NSArray arrayWithObjects:[[True new] autorelease], [[False new] autorelease], nil];
 }
 @end
 
