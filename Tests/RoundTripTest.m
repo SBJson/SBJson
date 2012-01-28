c937b4d5bce98bbb4d7df1977b909fc8a031aef3
upsuper <quanxunzhen@gmail.com>
2012-01-28 08:07:32 +0800

    Split test for NSData Category into a separate method

--- Tests/RoundTripTest.m
+++ Tests/RoundTripTest.m
@@ -84,33 +84,47 @@
 }
 
 
-- (void)testCategory {
+- (void)testStringCategory {
     [self foreachTestInSuite:@"Tests/Data/valid" apply:^(NSString *inpath, NSString *outpath) {
         NSError *error = nil;
-        NSString *strInput = [NSString stringWithContentsOfFile:inpath encoding:NSUTF8StringEncoding error:&error];
-        STAssertNotNil(strInput, @"%@ - %@", inpath, error);
+        NSString *input = [NSString stringWithContentsOfFile:inpath encoding:NSUTF8StringEncoding error:&error];
+        STAssertNotNil(input, @"%@ - %@", inpath, error);
 
         NSString *output = [NSString stringWithContentsOfFile:outpath encoding:NSUTF8StringEncoding error:&error];
         STAssertNotNil(output, @"%@ - %@", outpath, error);
 
-        id objFromStr = [strInput JSONValue];
-        STAssertNotNil(objFromStr, nil);
+        id object = [input JSONValue];
+        STAssertNotNil(object, nil);
 
-        NSString *json = [objFromStr JSONRepresentation];
+        NSString *json = [object JSONRepresentation];
         STAssertNotNil(json, nil);
 
         json = [json stringByAppendingString:@"\n"];
         STAssertEqualObjects(json, output, nil);
-        
-        NSData *dataInput = [NSData dataWithContentsOfFile:inpath];
-        STAssertNotNil(dataInput, @"%@", inpath);
-        
-        id objFromData = [dataInput JSONValue];
-        STAssertNotNil(objFromData, nil);
-        
-        STAssertEqualObjects(objFromData, objFromStr, nil);
     }];
 
+    STAssertEquals(count, (NSUInteger)16, nil);
+}
+
+- (void)testDataCategory {
+    [self foreachTestInSuite:@"Tests/Data/valid" apply:^(NSString *inpath, NSString *outpath) {
+        NSError *error = nil;
+        NSData *input = [NSData dataWithContentsOfFile:inpath];
+        STAssertNotNil(input, @"%@ - %@", inpath, error);
+        
+        NSString *output = [NSString stringWithContentsOfFile:outpath encoding:NSUTF8StringEncoding error:&error];
+        STAssertNotNil(output, @"%@ - %@", outpath, error);
+        
+        id object = [input JSONValue];
+        STAssertNotNil(object, nil);
+        
+        NSString *json = [object JSONRepresentation];
+        STAssertNotNil(json, nil);
+        
+        json = [json stringByAppendingString:@"\n"];
+        STAssertEqualObjects(json, output, nil);
+    }];
+    
     STAssertEquals(count, (NSUInteger)16, nil);
 }
 
