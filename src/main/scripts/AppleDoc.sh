a194db8766a5bff9499fdbd8905dd7c3ef7fb371
Stig Brautaset <stig@brautaset.org>
2012-12-26 00:58:12 +0000

    Moved docs around a bit to try and make subscribing to docsets work

--- src/main/scripts/AppleDoc.sh
+++ src/main/scripts/AppleDoc.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 VERS=3.2
+OUTPUT=~/Dropbox/DocSets/SBJson
 
 appledoc \
 --project-name "SBJson" \
+--project-version $VERS \
 --project-company "Stig Brautaset" \
 --company-id "org.brautaset" \
 --docset-atom-filename "SBJson.atom" \
---docset-feed-url "http://sbjson.org/api/$VERS/%DOCSETATOMFILENAME" \
---docset-package-url "http://sbjson.org/api/$VERS/%DOCSETPACKAGEFILENAME" \
---docset-fallback-url "http://sbjson.org/api/$VERS/" \
---output "../json-framework-pages/api/$VERS/" \
+--docset-feed-url "http://sbjson.org/api/%DOCSETATOMFILENAME" \
+--docset-package-url "http://sbjson.org/api/%DOCSETPACKAGEFILENAME" \
+--output $OUTPUT \
 --publish-docset \
 --logformat xcode \
 --keep-undocumented-objects \
@@ -21,3 +22,12 @@ appledoc \
 --ignore "*.m" \
 --index-desc "README.md" \
 Classes
+
+TARGET=../json-framework-pages/api
+cp $OUTPUT/publish/* $TARGET
+
+VTARGET=$TARGET/$VERS
+rm -rf $VTARGET
+cp -r $OUTPUT/html $VTARGET
+
+
