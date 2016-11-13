09b54f5d1e69b0ce97bf68f4ec58e41127608b8d
Stig Brautaset <stig@brautaset.org>
2016-11-13 21:18:48 +0000

    Fix bug in unwrapper code that would hide arrays(!)

--- Classes/SBJson5Parser.m
+++ Classes/SBJson5Parser.m
@@ -216,7 +216,7 @@ typedef enum {
 
 - (void)parserFoundArrayEnd {
     depth--;
-    if (depth > 1 || !supportPartialDocuments) {
+    if (depth > 0 || !supportPartialDocuments) {
 		id value = array;
 		[self pop];
         [self parserFound:value isValue:NO ];
